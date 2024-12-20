74302296895a9156026d41b64bfa93d10cff6ac4
apponislame <11appon11@gmail.com>
2024-12-21 02:17:51 +0600

    dist folder uploading for project host on vercel

--- dist/app/modules/blogs/blog.validation.js
+++ dist/app/modules/blogs/blog.validation.js
@@ -1,10 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogPostSchema = void 0;
+exports.blogUpdateSchema = exports.blogPostSchema = void 0;
 const zod_1 = require("zod");
 exports.blogPostSchema = zod_1.z.object({
-    title: zod_1.z.string().nonempty("Title is required").min(3, "Title must be at least 3 characters long"),
-    content: zod_1.z.string().nonempty("Content is required").min(10, "Content must be at least 10 characters long"),
-    author: zod_1.z.string().nonempty("Author ID is required"),
-    isPublished: zod_1.z.boolean().optional().default(true),
+    body: zod_1.z.object({
+        title: zod_1.z
+            .string({
+            required_error: "Title is required",
+        })
+            .min(3, "Title must be at least 3 characters long"),
+        content: zod_1.z
+            .string({
+            required_error: "Content is required",
+        })
+            .min(10, "Content must be at least 10 characters long"),
+        author: zod_1.z.string({
+            required_error: "Author ID is required",
+        }),
+    }),
+});
+exports.blogUpdateSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        title: zod_1.z.string().min(3, "Title must be at least 3 characters long").optional(),
+        content: zod_1.z.string().min(10, "Content must be at least 10 characters long").optional(),
+        author: zod_1.z.string().optional(),
+    }),
 });
